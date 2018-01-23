be7225baf0e5f93bafa8a794015025b172a063d0
loser <ralph.loser@intralot.de>
2018-01-23 11:44:10 +0100

    seperating configuration to Java Configuration

--- src/main/java/guru/springframework/services/PrimaryGermanGreetingService.java
+++ src/main/java/guru/springframework/services/PrimaryGermanGreetingService.java
@@ -1,6 +1,5 @@
 package guru.springframework.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
@@ -8,12 +7,13 @@ import org.springframework.stereotype.Service;
 /**
  * Created by jt on 5/24/17.
  */
-@Service
-@Profile("de")
-@Primary
 public class PrimaryGermanGreetingService implements GreetingService {
 
     private GreetingRepository greetingRepository;
+
+    public PrimaryGermanGreetingService( GreetingRepository greetingRepository ) {
+        this.greetingRepository = greetingRepository;
+    }
 
     @Override
     public String sayGreeting() {
